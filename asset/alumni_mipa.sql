ca169f763e7c5afcfa2187c22465a955c97c0967
Andika <Apratama918@gmail.com>
2020-01-13 05:18:28 +0700

    Update Database

--- asset/alumni_mipa.sql
+++ asset/alumni_mipa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2020 at 03:33 PM
+-- Generation Time: Jan 12, 2020 at 11:17 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -42,6 +42,7 @@ CREATE TABLE `alumni` (
   `No_Telepon` varchar(15) NOT NULL,
   `Email` varchar(250) NOT NULL,
   `Pekerjaan` varchar(250) NOT NULL,
+  `Nama_Pekerjaan` varchar(250) NOT NULL,
   `role` varchar(50) NOT NULL,
   `image` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -50,11 +51,8 @@ CREATE TABLE `alumni` (
 -- Dumping data for table `alumni`
 --
 
-INSERT INTO `alumni` (`Nim`, `Nama`, `Password`, `Jenis_Kelamin`, `Jurusan`, `Tahun_Masuk`, `Tahun_Keluar`, `Tempat_Lahir`, `Tanggal_Lahir`, `Alamat`, `No_Telepon`, `Email`, `Pekerjaan`, `role`, `image`) VALUES
-('1208107010018', 'Rijalul Muna', 'e5a85482d8b9bedbd68c39cb22aea751', 'L', 'Informatika', 2012, 2016, 'Banda Aceh', '1998-10-10', 'Komp Permata Lamyong, Banda Aceh', '08236718991', 'muna@gmail.com', 'Teknologi Informasi', 'Alumni', 'ce583b52689313827ce390e928214436.png'),
-('1408107010017', 'Siapa', '', 'L', 'Matematika', 2014, 2019, 'Dimana', '2097-01-06', 'Adadad', '08236718991', 'siapa@gmail.com', 'Apa Aja', 'Alumni', ''),
-('1508107010016', 'James Bones', '', 'L', 'Biologi', 2015, 2020, 'Minang', '1997-01-02', 'Dimana Aja', '08236718991', 'james@gmail.com', 'Bapak Rumah Tangga', 'Alumni', ''),
-('1508107010017', 'Mina Goroshi', '', 'P', 'Biologi', 2015, 2020, 'Tamiyang', '1997-01-02', 'Dimana Aja', '08236718991', 'minamina@gmail.com', 'IT Telkom', 'Alumni', '');
+INSERT INTO `alumni` (`Nim`, `Nama`, `Password`, `Jenis_Kelamin`, `Jurusan`, `Tahun_Masuk`, `Tahun_Keluar`, `Tempat_Lahir`, `Tanggal_Lahir`, `Alamat`, `No_Telepon`, `Email`, `Pekerjaan`, `Nama_Pekerjaan`, `role`, `image`) VALUES
+('1508107010017', 'Mina Goroshi', '', 'P', 'Biologi', 2015, 2020, 'Tamiyang', '1997-01-02', 'Dimana Aja', '08236718991', 'minamina@gmail.com', 'Scientist', 'Ahli Biologi', 'Alumni', '');
 
 -- --------------------------------------------------------
 
@@ -94,17 +92,18 @@ CREATE TABLE `mahasiswa` (
   `Password` varchar(250) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `image` varchar(250) NOT NULL,
-  `role` varchar(50) NOT NULL
+  `role` varchar(50) NOT NULL,
+  `Jurusan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`Nim`, `Nama`, `Password`, `Email`, `image`, `role`) VALUES
-('1608107010018', 'Andika Pratama', 'e5a85482d8b9bedbd68c39cb22aea751', 'apratama918@gmail.com', '5c63e7d4006726662e9a515f750c30f1.png', 'Mahasiswa'),
-('1708107010018', 'Andika Pratama', '74b87337454200d4d33f80c4663dc5e5', 'apratama918@gmail.com', '', 'Mahasiswa'),
-('1708107010034', 'Budi Gunawan', 'b0baee9d279d34fa1dfd71aadb908c3f', 'apratama918@gmail.com', '7d187b48ca1cb645124d9c8f5451cc4b.jpg', 'Mahasiswa');
+INSERT INTO `mahasiswa` (`Nim`, `Nama`, `Password`, `Email`, `image`, `role`, `Jurusan`) VALUES
+('1608107010018', 'Andika Pratama', 'e5a85482d8b9bedbd68c39cb22aea751', 'apratama918@gmail.com', '5c63e7d4006726662e9a515f750c30f1.png', 'Mahasiswa', ''),
+('1708107010018', 'Andika Pratama', '74b87337454200d4d33f80c4663dc5e5', 'apratama918@gmail.com', '', 'Mahasiswa', ''),
+('1708107010034', 'Budi Gunawan', 'b0baee9d279d34fa1dfd71aadb908c3f', 'apratama918@gmail.com', '7d187b48ca1cb645124d9c8f5451cc4b.jpg', 'Mahasiswa', '');
 
 -- --------------------------------------------------------
 
@@ -121,13 +120,6 @@ CREATE TABLE `p_perkerjaan` (
   `Keterangan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `p_perkerjaan`
---
-
-INSERT INTO `p_perkerjaan` (`id`, `Nama_Pekerjaan`, `Tahun_Masuk`, `Tahun_Keluar`, `Nim`, `Keterangan`) VALUES
-(1, 'SEO Tokopedia', 2015, 2018, '1208107010018', 'apasiih ini ?');
-
 -- --------------------------------------------------------
 
 --
@@ -142,13 +134,6 @@ CREATE TABLE `r_organisasi` (
   `Nama_Organisasi` varchar(250) NOT NULL,
   `Keterangan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `r_organisasi`
---
-
-INSERT INTO `r_organisasi` (`id`, `Nim`, `Tahun_Masuk`, `Tahun_Keluar`, `Nama_Organisasi`, `Keterangan`) VALUES
-(2, '1208107010018', 2012, 2016, 'BEM', 'apaapapapapaapapapapap');
 
 -- --------------------------------------------------------
 
@@ -165,13 +150,6 @@ CREATE TABLE `r_pendidikan` (
   `Kota` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `r_pendidikan`
---
-
-INSERT INTO `r_pendidikan` (`id`, `Nim`, `Tahun_Masuk`, `Tahun_Keluar`, `Nama_Instansi`, `Kota`) VALUES
-(1, '1208107010018', 2012, 2017, 'Unsyiah', 'Banda Aceh');
-
 -- --------------------------------------------------------
 
 --
@@ -185,13 +163,6 @@ CREATE TABLE `r_penghargaan` (
   `Tahun` int(11) NOT NULL,
   `Keterangan` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `r_penghargaan`
---
-
-INSERT INTO `r_penghargaan` (`id`, `Nim`, `Nama_Penghargaan`, `Tahun`, `Keterangan`) VALUES
-(1, '1208107010018', 'Award', 2015, 'andika pratama');
 
 --
 -- Indexes for dumped tables
@@ -253,25 +224,25 @@ ALTER TABLE `jurusan`
 -- AUTO_INCREMENT for table `p_perkerjaan`
 --
 ALTER TABLE `p_perkerjaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `r_organisasi`
 --
 ALTER TABLE `r_organisasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `r_pendidikan`
 --
 ALTER TABLE `r_pendidikan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `r_penghargaan`
 --
 ALTER TABLE `r_penghargaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
